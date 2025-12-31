009cb3cca898dc5975fee674f63dd24b35431660
Hisham <linkedin.hisham@gmail.com>
2025-12-31 11:53:44 +0530

    Added delete functionality to remove languages which dosent have any data in the stats. changed the delete icon.

--- javascript/add_new_language.js
+++ javascript/add_new_language.js
@@ -113,16 +113,25 @@ function renderLanguages(langName, langKey) {
       section.closest("form").id === "previous-total-form";
     const prefix = isPreviousSection ? previousPrefix : todaysPrefix;
     const inputId = `${prefix}-${langKey}`;
+    // add delete button only for previous totals
+    const deleteButton = isPreviousSection
+      ? `<button type="button" class="delete-lang" data-lang="${langKey}">
+            <?xml version="1.0" encoding="utf-8"?><!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
+            <svg width="20px"  viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M6 5H18M9 5V5C10.5769 3.16026 13.4231 3.16026 15 5V5M9 20H15C16.1046 20 17 19.1046 17 18V9C17 8.44772 16.5523 8 16 8H8C7.44772 8 7 8.44772 7 9V18C7 19.1046 7.89543 20 9 20Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+         </button>`
+      : "";
     formItem.innerHTML = `
         <label for="${inputId}">${langName}</label>
         <input type="number" id="${inputId}" name="${inputId}" data-lang="${langKey}" placeholder="10" min="0"
         title="Total lines of ${langName} code written">
+        ${deleteButton}
         `;
-    //  <button type="button" class="delete-lang" data-lang="${languageName}">x</button>
     section.appendChild(formItem);
   });
 
-  // attachDeleteHandlers();
+  attachDeleteHandlers();
 }
 
 // for everly languages added by the user rendering it while page load
@@ -147,31 +156,33 @@ function getUserLanguages() {
   );
 }
 
-/* 
-DELETING A LANGUAGE WOULD MAKE TOTAL MISMATCH IN DAILY LOGS SUM VS PREVIOUS TOTALS.
-SO DELETING A LANGUAGE MUST ALSO DELETE THE VALUES IN DAILY STATS , WILL IMPLEMENT IT LATER
-note : add the delete button while rendering the languages , also add delete handlers function call when rendering
+/* DELETING A LANGUAGE WOULD MAKE TOTAL MISMATCH IN DAILY LOGS SUM VS PREVIOUS TOTALS.
+SO ONLY DELETE LANGUAGES WHICH HAS NO DATA SAVED
+note : add the delete button while rendering the languages , also add delete handlers function call when rendering */
 
 // function to delete language
-function deleteALanguage(languageName) {
-  let savedLanguages =
-    JSON.parse(localStorage.getItem(storage_key_user_set_language_array)) || [];
+function deleteALanguage(langKey) {
+  let userLanguages = getUserLanguages();
 
-  if (savedLanguages.length === 0) {
+  if (userLanguages.length === 0) {
+    return;
+  }
+  // if language conatins data in the stats then return
+  if (hasLanguageData(langKey)) {
+    showSnackBar(`Cannot Delete! ${langKey} has recorded data in stats `, true);
     return;
   }
   //removing the seletect language form the saved languages array
-  savedLanguages = savedLanguages.filter((lang) => lang.key !== languageName);
+  userLanguages = userLanguages.filter((lang) => lang.key !== langKey);
   localStorage.setItem(
     storage_key_user_set_language_array,
-    JSON.stringify(savedLanguages)
+    JSON.stringify(userLanguages)
   );
   // deleting the lan from ui
-  document
-    .querySelectorAll(`[data-lang="${languageName}"]`)
-    .forEach((languages) => {
-      languages.closest(".form-item").remove();
-    });
+  document.querySelectorAll(`[data-lang="${langKey}"]`).forEach((languages) => {
+    languages.closest(".form-item").remove();
+  });
+  showSnackBar(`Successfully removed - ${langKey}`);
 }
 
 // adding event listners for every new x / delete button
@@ -186,4 +197,33 @@ function attachDeleteHandlers() {
     });
   });
 }
- */
+
+// function to check if the language has any data anywhere in the stats before deleting it
+function hasLanguageData(langKey) {
+  const dailyLogs = fetchDailyLogs();
+  const dailyLogsSum = fetchDailyLogsSum();
+  const previousTotals = fetchPreviousPlusDaily();
+
+  // daily logs returns an array of objects
+  if (
+    Array.isArray(dailyLogs) &&
+    dailyLogs.some((stats) => Number(stats[langKey]) > 0)
+  ) {
+    console.log("daily logs contains data for", langKey);
+
+    return true;
+  }
+
+  if (Number(dailyLogsSum?.[langKey]) > 0) return true;
+
+  // previous totals is array when empty , object when has data
+  if (
+    (Array.isArray(previousTotals) &&
+      previousTotals.some((stats) => Number(stats[langKey]) > 0)) ||
+    Number(previousTotals?.[langKey]) > 0
+  )
+    return true;
+
+  // return false if data does not exists and its safe to delete.
+  return false;
+}
